b3c8fc943d960bc3c7a821fd1ba88ec7fa450b60
Your Name <you@example.com>
2019-11-22 00:14:21 +0800

    two

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
+    <tab-bar>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.png" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/tabbar/profile.png" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from 'components/tabbar/TabBar'
+import TabBarItem from 'components/tabbar/TabBarItem'
 
 
 export default {
   name: 'app',
   components: {
-   
+   TabBar,
+   TabBarItem
   }
 }
 </script>
 
-<style>
+<style scoped>
   @import 'assets/css/base.css';
+
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   configureWebpack:{
     resolve:{
       alias:{
-        'assets':'@/assets',
+      'assets':'@/assets',
       'common':'@/common',
       'components':'@/components',
       'network':'@/network',
